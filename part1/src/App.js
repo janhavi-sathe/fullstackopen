f55532742354e04e830eef0589c68e330032d293
Janhavi Sathe <32736553+janhavi-sathe@users.noreply.github.com>
2022-02-21 11:32:41 +0530

    Exercise 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -14,7 +14,7 @@ const Part = ({ part }) => {
 
 const Content = ({ parts }) => {
   const allParts = parts.map(part => <Part part={part}/>);
-  console.log(allParts);
+  // console.log(allParts);
   return (
     <div>
       {allParts}
@@ -35,31 +35,32 @@ const Total = ({ parts }) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
-      <Header coursename={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header coursename={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
 export default App
-
